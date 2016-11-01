0d4fdc28ccd5775bc3bf7dbee32602fb431fe7d1
Colin Bendell <colin@bendell.ca>
2016-10-31 23:43:11 -0400

    updated package definition

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    APIClient: require('./src/apiclient'),
+    WebSite: require('./src/website'),
+    DNS: require('./src/dns'),
+    TLS: require('./src/tls'),
+    Luna: require('./src/luna'),
+};
