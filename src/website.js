bfdc9e8a6bf6c73e31095ef9bb8c280ac8fd6114
Hunter, Kirsten <khunter@akamai.com>
2017-08-16 09:12:17 -0700

    Lowering the throttle to avoid hitting rate limits

--- src/website.js
+++ src/website.js
@@ -20,7 +20,7 @@ let fs = require('fs');
 let tmpDir = require('os').tmpdir();
 
 let concurrent_requests = 0;
-let request_throttle = 50;
+let request_throttle = 25;
 let cache_complete = 0;
 
 //export
