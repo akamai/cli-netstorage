30958bd7e456e578cc0cdb1098c3ba2b93eaa873
Kirsten Hunter <synedra@gmail.com>
2018-01-03 15:42:20 -0800

    Some laggardly modules

--- src/logger.js
+++ src/logger.js
@@ -16,7 +16,7 @@ var log4js = require('log4js'),
     logger = log4js.getLogger();
 
 if (!process.env.LOG4JS_CONFIG) {
-  logger.setLevel(log4js.levels.ERROR);
+  logger.level = log4js.levels.ERROR;
 }
 
 module.exports = logger;
