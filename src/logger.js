c46015f6792d672f63354efbd35ba7e09ea4a175
Jorge Sosa <jorges@oktana.io>
2018-05-31 08:29:57 -0300

    Linting logger.js

--- src/logger.js
+++ src/logger.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 'use strict';
-var log4js = require('log4js'),
-  logger = log4js.getLogger();
+const log4js = require('log4js');
+const logger = log4js.getLogger();
 
 if (!process.env.LOG4JS_CONFIG) {
   logger.level = log4js.levels.ERROR;
