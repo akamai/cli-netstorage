cf12225c81910b8786b4219893b200931972510a
Jorge Sosa <jorges@oktana.io>
2018-05-31 09:16:15 -0300

    Linting netstorage_rc

--- src/netstorage_rc.js
+++ src/netstorage_rc.js
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 'use strict';
-var fs = require('fs');
-helpers = require('./helpers');
+const helpers = require('./helpers');
 
 function readEnv(section) {
   // If any are set, we're committed
